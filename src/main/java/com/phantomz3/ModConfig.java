9b40605deee17bc53e831a65abce7fbdd9510d9d
Ace10-1 <bryantlouis29@gmail.com>
2025-01-13 17:28:20 +0700

    Update ModConfig.java

--- src/main/java/com/phantomz3/ModConfig.java
+++ src/main/java/com/phantomz3/ModConfig.java
@@ -6,11 +6,11 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = LifestealMod.MOD_ID)
 public class ModConfig implements ConfigData {
     public int maxHeartCap = 40;
-    public boolean disableEnderPearl = true;
-    public boolean disableCPVP = true;
-    public boolean disableTotem = true;
-    public boolean disableNetherite = true;
+    public boolean disableEnderPearl = false;
+    public boolean disableCPVP = false;
+    public boolean disableTotem = false;
+    public boolean disableNetherite = false;
     public boolean noDragonEggEnderChest = true;
-    public int riptideCooldown = 200; // 200 ticks = 10 seconds
+    public int riptideCooldown = 100; // 100 ticks = 5 seconds
     public boolean riptideCooldownEnabled = true;
 }
